296be11fa68028b6af42ba38fe8499dbac5a2b42
ko kristin <koberlander115@gmail.com>
2019-01-31 18:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ function theBeatlesPlay(musician, instrument){
   return list;
 }
 
-// const facts = [
-//   "He was the last Beatle to learn to drive",
-//   "He was never a vegetarian",
-//   "He was a choir boy and boy scout",
-//   "He hated the sound of his own voice"
-// ];
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
-// function johnLennonFacts(facts){
-//   var list = [];
-//   var i = 0;
-//   while (i <= 3){
-//     list.push(`${facts[i]}!!!`);
-//     i++;
-//   }
-//   return list;
-// }
+function johnLennonFacts(facts){
+  var list = [];
+  var i = 0;
+  while (i <= 3){
+    list.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return list;
+}
 
 function iLoveTheBeatles(n){
  var list = [];
