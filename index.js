f59e54b64e6b09f22ea80e1eaf6fb3c05445fa30
ko kristin <koberlander115@gmail.com>
2019-01-31 18:28:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ function theBeatlesPlay(musician, instrument){
   return list;
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 function johnLennonFacts(facts){
   var list = [];
